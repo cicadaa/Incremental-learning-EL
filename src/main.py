5a69f8bbaab42d4fa4cd569b8c3a02aacf409f64
cicadaaa <cicaday@outlook.com>
2021-05-27 16:56:36 +0200

    added lazy function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from distutils import version
+# from distutils import version
 from numpy import record
 from torch.nn.modules.module import T
 from .config import LocalConfig
 from .runner import Runner
-from .models import SVRModel, OSVRModel, LSTM
+from .models import SVRModel, OSVRModel, OLSTM
 from .dataset import Dataset
 import logging
 
@@ -20,19 +20,16 @@ if __name__ == "__main__":
     shiftRange = LocalConfig.shiftRange
     removeFeatures = LocalConfig.removeFeatures
 
-    # model = OSVRModel(learning_rate='constant', eta0=0.3,
-    #                   loss='epsilon_insensitive', penalty='l2')
+    osvr = OSVRModel(learning_rate='constant', eta0=0.3,
+                      loss='epsilon_insensitive', penalty='l2')
 
-    learning_rate = 0.002 #best rate
-    input_size = 41
-    hidden_size = 320
-    num_layers = 1
-    num_classes = 1
+    olstm = OLSTM(num_classes=1, input_size=1, hidden_size=128, num_layers=1)
+
+    learning_rate = 0.01 #best rate
+
 
     dataset = Dataset(dataPath=dataPath, shiftFeatures=['meter'], categoryFeatures=['dayOfYear','hourOfDay','dayOfWeek','holiday','weekend'],
-                      shiftRange=shiftRange, removeFeatures=removeFeatures, isTorch=True)
-  
-    lstm = LSTM(num_classes, input_size, hidden_size, num_layers)
-    # print(lstm)
-    runner = Runner(warmStartPoint=1, dataset=dataset, model=lstm, deep=True, learningRate=learning_rate)
-    runner.run(duration=10, interval=0, name='OLSTM', plot=True, record=True, verbose=True)
+                      shiftRange=shiftRange, removeFeatures=removeFeatures, isTorch=False)
+    
+    runner = Runner(warmStartPoint=1, dataset=dataset, model=osvr, deep=False, learningRate=learning_rate, lazy=False)
+    runner.run(duration=0.1, interval=0, name='OSVR', plot=True, record=True, verbose=False)
